018c58edac2c5aa7fc16c2e0423b1aff93e7d386
Connor McDermid <cam-o-man@live.com>
2019-09-09 13:05:31 -0400

    Initial Commit

---
+++ Carlab/src/hendricks/carlab/Main.java
@@ -0,0 +1,71 @@
+package hendricks.carlab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //implement car
+    }
+}
+
+
+class Car {
+    private int fuel = 0;
+    private final int maxfuel = 70;
+    private int mpg;
+    private int mileage;
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public Car(int mpg) {
+        this.mpg = mpg;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getMaxfuel() {
+        return maxfuel;
+    }
+
+    public int getMpg() {
+        return mpg;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "fuel=" + fuel +
+                ", maxfuel=" + maxfuel +
+                ", mpg=" + mpg +
+                '}';
+    }
+    public void addFuel(int added) {
+        int totalFuel = this.getFuel() + added;
+        if (totalFuel > this.getMaxfuel()) {
+            System.out.println("That's more fuel than your car can hold. You might want to rethink this.");
+        } else {
+            this.setFuel(totalFuel);
+            System.out.printf("Added %s gallons of fuel\n", added);
+        }
+    }
+    public boolean drive(int miles) {
+        int pergallon = this.getMpg();
+        int fuel = this.getFuel();
+        int mileage = this.getMileage();
+        if (miles/pergallon <= fuel) {
+            fuel -= miles/pergallon;
+            mileage += miles;
+        }
+    }
+}
