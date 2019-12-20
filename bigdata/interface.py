3722b8c4b1e4e74eab2834fbd7cbdb4ec15281ae
Connor McDermid <cam-o-man@live.com>
2019-12-19 21:00:37 -0500

    bigdata lab complete

--- bigdata/interface.py
+++ bigdata/interface.py
@@ -1,17 +1,24 @@
 from typing import Union
 from typing import Optional
+import name_parser
 
-def acquire_user_request() -> str:
+
+def acquire_user_request() -> int:
     print("Please input the data you need.\nWould you like to know:\n"
           + "1. \tThe most popular name overall?\n"
           + "2. \tThe most popular name of a specific gender?\n"
           + "3. \tThe least popular name?\n"
           + "4. \tHow popular a specific name is?\n"
           + "5. \tExit")
-    usr: str = input("Please enter your choice's number: ")
+    usr: int = int(input("Please enter your choice's number: "))
 
     return usr
 
-def return_request(request: Union[int, str]) -> Optional[str]:
-    print(request)
-    # Continue from here TODO: configure request and pseudoshell system
+
+def fulfill():
+    usr: int = acquire_user_request() - 1  # subtracting one ensures the request is 0-indexed
+    print(name_parser.run(usr))
+
+
+if __name__ == "__main__":
+    fulfill()
--- bigdata/name_parser.py
+++ bigdata/name_parser.py
@@ -3,16 +3,51 @@ Name: Connor McDermid
 Lab: Python 11
 Extra: CSV Files
 """
+from typing import Union
 
-import csv
 
-csvfile = open("data.csv", newline='')
+def run(request: int) -> Union[tuple, str]:
+    import csv
 
-reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    csvfile = open("data.csv", newline='')
 
-index = 0
-for row in reader:
-    print(row)
-    index += 1
-    if index >= 10:
-        break
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+
+    index = 0
+
+    if request == 0:
+        for idx, row in enumerate(reader):
+            if idx == 1:
+                return row[1]
+    elif request == 1:
+        usr: int = int(input("Male[1] or Female[2]? "))
+        if usr == 1:
+            for row in reader:
+                if row[3] != "boy":
+                    continue
+                return row[1]
+        elif usr == 2:
+            for row in reader:
+                if row[3] != "girl":
+                    continue
+                return row[1]
+
+    elif request == 2:
+        next(reader)
+        for row in reader:
+            if float(row[2]) == 0.000127:
+                return row[1]
+
+    elif request == 3:
+        usr: str = input("Which name would you like to grep for? ")
+        for row in reader:
+            if row[1] != usr:
+                continue
+            csvfile.seek(0)  # Resets the CSV read value to 0
+            return "{}\n{}".format(next(reader), row)
+
+    elif request == 4:
+        exit(0)
+
+    else:
+        return "Request code {} was not a valid request.".format(request)
